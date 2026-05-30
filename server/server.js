4d656bb479c56f02609d6fe6a26a1fccb0b7ac86
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2026-05-30 07:34:16 +0000

    update

--- server/server.js
+++ server/server.js
@@ -19,13 +19,13 @@ app.post('/api/post', (req, res)=>{
     headerID = req.headers['headerID'];
     //req.query
     param = req.query.paramID
-        
+    console.log("Data log from User : ", body, headers, headerID, param)    
     res.json({
         'message':'post',
         'body':body,
         'headers': headers,
         'headerID':headerID,
         'param':param
-    })
+    });
 
 });
